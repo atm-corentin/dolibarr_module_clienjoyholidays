1e090e713024b43fdc7fa70348be01811346afe4
atm-corentin <corentin.carayon@atm-consulting.fr>
2024-04-26 10:17:49 +0200

    Object link in progress

--- class/actions_clienjoyholidays.class.php
+++ class/actions_clienjoyholidays.class.php
@@ -46,27 +46,36 @@ class ActionsCliEnjoyHolidays
 	/**
 	 * Constructor
 	 */
-	public function __construct()
+	public function __construct($db)
 	{
+		$this->db = $db;
 	}
+
 
 	/**
 	 * Overloading the doActions function : replacing the parent's function with the one below
 	 *
-	 * @param   array()         $parameters     Hook metadatas (context, etc...)
-	 * @param   CommonObject    &$object        The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
-	 * @param   string          &$action        Current action (if set). Generally create or edit or null
-	 * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
+	 * @param array()         $parameters     Hook metadatas (context, etc...)
+	 * @param CommonObject    &$object The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
+	 * @param string          &$action Current action (if set). Generally create or edit or null
+	 * @param HookManager $hookmanager Hook manager propagated to allow calling another hook
 	 * @return  int                             < 0 on error, 0 on success, 1 to replace standard code
 	 */
-	function addMoreActionsButtons($parameters, &$object, &$action, $hookmanager) {
+	function addMoreActionsButtons($parameters, &$object, &$action, $hookmanager)
+	{
 		global $langs;
 
 
-		if (in_array('propalcard', explode(':', $parameters['context'])) )
-		{
-			print '<a class="butAction" href="'.DOL_URL_ROOT.'/custom/clienjoyholidays/clienjoyholidays_card.php?action=create&origin='.$object->element.'&originid='.$object->id.'&socid='.$object->socid.'">'.$langs->trans("CreateCliEnjoyHolidays").'</a>';
+		if (in_array('propalcard', explode(':', $parameters['context']))) {
+			print '<a class="butAction" href="' . DOL_URL_ROOT . '/custom/clienjoyholidays/clienjoyholidays_card.php?action=create&origin=' . $object->element . '&originid=' . $object->id . '&socid=' . $object->socid . '">' . $langs->trans("CEHCreate") . '</a>';
+
+
 		}
+
+	}
+
+	function doActions($parameters, &$object, &$action, $hookmanager)
+	{
 
 	}
 
